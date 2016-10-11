c64cf302cd922f572d6efbac77371e39cfa2c8c8
hjy5@uw.edu <hjy5@uw.edu>
2016-10-11 15:13:29 -0700

    finished exercise

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -1,38 +1,45 @@
 # Exercise 4: Working with Data Frames
 
 # Load R's "USPersonalExpenditure" dataest using the `data()` function
-
+data('USPersonalExpenditure')
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately, it's not a data.frame
 # Test this using the is.data.frame function
-
+as.data.frame('USPersonalExpenditure')
 
 # Luckily, you can simply pass the USPersonalExpenditure variable to the data.frame function
 # to convert it a data.farme
+data.frame('USPersonalExpenditure')
 
 # Create a new variable by passing the USPersonalExpenditure to the data.frame function
-
+US.personal.exp <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(US.personal.exp)
+[1] "X1940" "X1945" "X1950" "X1955" "X1960"
 
 # Why are they so strange?
 
 
 # What are the row names of your dataframe?
-
+rownames(US.personal.exp)
+[1] "Food and Tobacco"    "Household Operation" "Medical and Health" 
+[4] "Personal Care"       "Private Education"
 
 # Create a column `category` that is equal to your rownames
-
+US.personal.exp$category <- rownames(US.personal.exp)
 
 # How much money was spent on personal care in 1940?
-
+US.personal.exp['Personal Care', 'X1940']
+[1] 1.04
 
 # How much money was spent on Food and Tobacco in 1960
-
+US.personal.exp['Food and Tobacco', 'X1960']
+[1] 86.8
 
 # What was the highest expenditure category in 1960?
-
+max(US.personal.exp$X1960)
+[1] 86.8
 
 ### Bonus ###
 
